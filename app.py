88a8c697ad21a2507e8e827617919b38afa51a1c
rvibek <rvibek@gmail.com>
2019-06-16 11:03:59 +0200

    encrypted caseno

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request
+from flask_bcrypt import Bcrypt
 from data import Statuses
 
 app = Flask(__name__)
 
+bcrypt = Bcrypt()
 Statuses = Statuses()
 
 
@@ -20,8 +22,20 @@ def status():
 
 @app.route('/status/<string:caseno>/')
 def statusDetail(caseno):
-    data = [v for v in Statuses if v["caseno"] == caseno][0]
+    # data = [v for v in Statuses if v["caseno"] == caseno][0]
+    data = [v for v in Statuses if bcrypt.check_password_hash(v['crypt_caseno'], caseno)][0]
     return render_template('statusdetail.html', data=data)
+
+
+@app.route('/status_check', methods=['GET', 'POST'])
+def statusCheck():
+    if request.method == 'POST':
+        caseno = request.form['caseno']
+        try:
+            data = [v for v in Statuses if bcrypt.check_password_hash(v['crypt_caseno'], caseno)][0]
+            return render_template('statusdetail.html', data=data)
+        except IndexError:
+            return "Nothing to display"
 
 
 if __name__ == '__main__':
--- data.py
+++ data.py
@@ -2,15 +2,15 @@ def Statuses():
     assistances = [
         {
             'id': 1,
-            'caseno': '001-01C0001',
+            'crypt_caseno': '$2b$12$4zlhqE0xwD9lyIrF3wTAHOT03KCUL/WZQ3oEvTLJhLnNB1vwfjPzq',
             'dob': '1990-01-01',
             'phone': '01095550001',
             'details': [{
-                'type': 'education',
-                'individualid': '001-01C0001_01',
-                'amount': 650,
-                'collectioncenter': 'PO 001 Cairo',
-                'collectiondate': '2019-06-30'
+                            'type': 'education',
+                            'individualid': '001-01C0001_01',
+                            'amount': 650,
+                            'collectioncenter': 'PO 001 Cairo',
+                            'collectiondate': '2019-06-30'
             },
                 {
                 'type': 'education',
@@ -29,15 +29,15 @@ def Statuses():
         },
         {
             'id': 2,
-            'caseno': '001-01C0002',
+            'crypt_caseno': '$2b$12$Mzu3..JhF6/vlL9yJhHPDOgcAAUaNpKVQ.bP79Vby.UQ/qysrz8U.',
             'dob': '1990-02-02',
             'phone': '01095550002',
             'details': [{
-                'type': 'education',
-                'individualid': '001-01C0002_01',
-                        'amount': 650,
-                        'collectioncenter': 'PO 001 Cairo',
-                'collectiondate': '2019-06-30'
+                            'type': 'education',
+                            'individualid': '001-01C0002_01',
+                            'amount': 650,
+                            'collectioncenter': 'PO 001 Cairo',
+                            'collectiondate': '2019-06-30'
             },
                 {
                 'type': 'maternity',
@@ -49,15 +49,15 @@ def Statuses():
         },
         {
             'id': 3,
-            'caseno': '001-01C0003',
+            'crypt_caseno': '$2b$12$0sC/lJwV5u3Bzgjoyl0s8Obl6Nrxhcn3YSNfoIAcz6wTbr1tv/4Je',
             'dob': '1990-03-03',
             'phone': '01095550003',
             'details': [{
-                'type': 'maternity',
-                'individualid': '001-01C0003_01',
-                        'amount': 1000,
-                        'collectioncenter': 'PO 002 Alexandria',
-                'collectiondate': '2019-12-31'
+                            'type': 'maternity',
+                            'individualid': '001-01C0003_01',
+                            'amount': 1000,
+                            'collectioncenter': 'PO 002 Alexandria',
+                            'collectiondate': '2019-12-31'
             },
                 {
                 'type': 'health',
@@ -70,15 +70,15 @@ def Statuses():
 
         {
             'id': 4,
-            'caseno': '001-01C0004',
+            'crypt_caseno': '$2b$12$D3f8a9CKh681TSQAkRk49uGbruBjaGaTjMXWdvkFMXkPCYnt8syZa',
             'dob': '1990-04-04',
             'phone': '01095550004',
             'details': [{
-                'type': 'food',
-                'individualid': '001-01C0004_01',
-                        'amount': 1200,
-                        'collectioncenter': 'PO 010 Cairo',
-                'collectiondate': '2020-12-31'
+                            'type': 'food',
+                            'individualid': '001-01C0004_01',
+                            'amount': 1200,
+                            'collectioncenter': 'PO 010 Cairo',
+                            'collectiondate': '2020-12-31'
             }]
         }
     ]
